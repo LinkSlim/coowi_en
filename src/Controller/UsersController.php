5d40a871d43558c7a59f241539a8d86f4e8a6b4e
Jesusmmz <jesusmmz17@hotmail.com>
2016-04-06 17:28:11 +0200

    Usuario se redirige a su View tras editar sus datos de usuario

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -90,7 +90,7 @@ class UsersController extends AppController
             $user = $this->Users->patchEntity($user, $this->request->data);
             if ($this->Users->save($user)) {
                 $this->Flash->success(__('The user has been saved.'));
-                return $this->redirect(['action' => 'index']);
+                return $this->redirect(['action' => 'view', $id]);
             } else {
                 $this->Flash->error(__('The user could not be saved. Please, try again.'));
             }
